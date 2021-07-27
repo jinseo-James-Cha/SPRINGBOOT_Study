9ac8c8a2ac20e9e3975fc4287cb48e03119a8db6
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-28 04:08:28 +0900

    주석 첨부

--- src/main/java/com/chap2/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/chap2/book/springboot/web/dto/HelloResponseDto.java
@@ -3,8 +3,8 @@ package com.chap2.book.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
+@Getter // getter를 생성해 준다
+@RequiredArgsConstructor // 선언된 모든 final필드가 포함된 생성자를 만들어줌, final이 없이면 포함하지않는다.
 public class HelloResponseDto {
 
     private final String name;
