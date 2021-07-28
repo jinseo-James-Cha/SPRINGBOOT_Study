f033a7dc4102923047e1df693306d6783888013e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-29 05:19:40 +0900

    2021-07-29 api CRUD completed and view as well

--- src/main/java/com/chap2/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/chap2/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,14 @@
 package com.chap2.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // 반드시 @Entity Posts클래스와 같이 움직여야 함
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/com/chap2/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/chap2/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.chap2.book.springboot.service.posts;
 
 import com.chap2.book.springboot.domain.posts.Posts;
 import com.chap2.book.springboot.domain.posts.PostsRepository;
+import com.chap2.book.springboot.web.dto.PostsListResponseDto;
 import com.chap2.book.springboot.web.dto.PostsResponseDto;
 import com.chap2.book.springboot.web.dto.PostsSaveRequestDto;
 import com.chap2.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // 생성자를 통한 DI
 @Service
@@ -35,10 +38,26 @@ public class PostsService {
     }
 
     @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                                       .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                                            .map(PostsListResponseDto::new)//.map(posts -> new PostListResponseDto(posts))
+                                            .collect(Collectors.toList());
+    }
+
 }
