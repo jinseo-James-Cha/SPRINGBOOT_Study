b02edd1728bb842ffcc752180c3ec593abc9b60e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-29 18:55:16 +0900

    2021-07-29 naver 로그인 연동 시작

--- src/main/java/com/chap2/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/chap2/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,8 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes){
 
+
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
