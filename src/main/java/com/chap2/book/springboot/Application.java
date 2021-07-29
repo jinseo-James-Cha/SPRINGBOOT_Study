4370b0c954507a19ceaa7951818dcb37ea1455bb
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-29 23:23:19 +0900

    2021-07-29 naver 로그인 연동 완료, JpaConfig 분리 작업 완료

--- src/main/java/com/chap2/book/springboot/Application.java
+++ src/main/java/com/chap2/book/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing 분리됨 -> config 패키지의 JpaConfig
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
