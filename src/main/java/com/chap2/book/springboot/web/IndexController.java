2714e3c3b016c427d2fb1aa5726d297f88525676
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-07-29 18:16:14 +0900

    2021-07-29 google 로그인 성공

--- src/main/java/com/chap2/book/springboot/web/IndexController.java
+++ src/main/java/com/chap2/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.chap2.book.springboot.web;
 
+import com.chap2.book.springboot.config.auth.LoginUser;
+import com.chap2.book.springboot.config.auth.dto.SessionUser;
 import com.chap2.book.springboot.service.posts.PostsService;
 import com.chap2.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+//    private final HttpSession httpSession; @LoginUser로 대체
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); @LoginUser 로 대체
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
