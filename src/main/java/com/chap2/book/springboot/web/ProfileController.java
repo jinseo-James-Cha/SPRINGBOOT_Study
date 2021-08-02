e764d324142f9cc4bd8a0014a32774069b075555
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-02 21:13:24 +0900

    2021-08-02 500 internal error, fixed ProfileController.java

--- src/main/java/com/chap2/book/springboot/web/ProfileController.java
+++ src/main/java/com/chap2/book/springboot/web/ProfileController.java
@@ -5,24 +5,24 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
 public class ProfileController {
-
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
-
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        String defaultProfiles = profiles.isEmpty()? "default" : profiles.get(0);
-
-        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfiles);
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
